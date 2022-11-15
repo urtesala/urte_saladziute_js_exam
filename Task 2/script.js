45d38caec2a977a3509a7cc63ccb2375f9d04ed9
urtesala <urtesaladziute@gmail.com>
2022-11-15 09:55:44 +0300

    task 2 - clicker

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,13 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+const clickMe = document.getElementById("btn__element");
+const clickCount = document.getElementById("btn__state");
+
+let count = 0;
+
+clickMe.onclick = function () {
+  count += 1;
+  clickCount.innerHTML = count;
+};
