9c21a48cf2a5868096c9f19c1c56b3fafb966a03
urtesala <urtesaladziute@gmail.com>
2022-11-15 09:46:12 +0300

    task 1 - weight converter done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,21 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+// DOM elements
+
+const valueEl = document.getElementById("search");
+const submitEl = document.getElementById("submit-btn");
+const outputEl = document.getElementById("output");
+
+submitEl.addEventListener("click", function (e) {
+  e.preventDefault();
+  let kg = valueEl.value;
+  pounds = kg * 2.2046;
+  grams = kg / 0.001;
+  oz = kg * 35.274;
+  outputEl.innerHTML = `<p>${kg} kilogramai yra ${pounds} svarai</p>
+  <p>${kg} kilogramai yra ${grams} gramai</p>
+  <p>${kg} kilogramai yra ${oz} uncijos</p>
+  `;
+});
